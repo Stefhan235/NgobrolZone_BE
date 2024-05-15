2d73d98a39a4f764b811a701ea95e11867b906b9
aisyahwlandr <aisyahwlandr@gmail.com>
2024-05-15 15:54:09 +0700

    fix route

--- controller/profile.js
+++ controller/profile.js
@@ -2,9 +2,8 @@ const { getProfiles } = require("../usecase/profile");
 
 exports.getProfiles = async (req, res, next) => {
     try {
-        const { user_id } = req.params
         // get user by id
-        const data = await getProfiles(+user_id)
+        const data = await getProfiles(req?.user?.id)
 
         res.status(200).json({
             message: "Success",
